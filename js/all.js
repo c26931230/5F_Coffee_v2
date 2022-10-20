400f94ac4cb0459ea6c3191a0bb8e9abd6d12f35
Mandy_Li <c26931230@gmail.com>
2022-10-20 22:46:48 +0800

    index js 動畫

--- js/all.js
+++ js/all.js
@@ -1,22 +1,142 @@
+// ------index.html banner動畫----
+window.onload = function(){
+    let banner_slidein = document.getElementsByClassName("banner_slidein");
+    let banner_fadein = document.getElementsByClassName("banner_fadein");
+    for(let i=0; i < banner_slidein.length; i++){
+        banner_slidein[i].classList.add("active");
+    }
+    for(let i=0; i < banner_fadein.length; i++){
+        banner_fadein[i].classList.add("active");
+    }
+}
+// -----index.html slogan動畫 jQuery-----
+$(document).ready(function(){
+    $(window).ready(function(){
+        carousel(1)
+    })
+    // scroll事件發生
+    $(window).scroll(function(){
+        slideIn()
+        fadeIn()
+        new_arrivals()
+        schoolLocation()
+        shopLoaction()
+        loacationTitle()
+        discount()
+    })
+    // -----slogan 動畫-----
+    function slideIn(){
+        let outerHeight = $('#slogan_img').outerHeight();
+        let offsetTop = $('#slogan_img').offset().top;
+        if(scrollY + innerHeight - outerHeight / 3 > offsetTop){
+            $('#slogan_img').addClass('active')
+        }else{
+            $('#slogan_img').removeClass('active')
+        }
+    }
+    function fadeIn(){
+        let outerHeight = $('#slogan_txt').outerHeight();
+        let offsetTop = $('#slogan_txt').offset().top;
+        if(scrollY + innerHeight - outerHeight / 3 > offsetTop){
+            $('#slogan_txt').addClass('active')
+        }else{
+            $('#slogan_txt').removeClass('active')
+        }
+    }
+    // -----new arrivel動畫-----
+    function new_arrivals(){
+        let outerHeight = $('.new_arrivals > .title_box').outerHeight();
+        let offsetTop = $('.new_arrivals > .title_box').offset().top;
+        if(scrollY + innerHeight - outerHeight * 2 > offsetTop){
+            $('.new_arrivals .title_box').addClass('active')
+        }else{
+            $('.new_arrivals .title_box').removeClass('active')
+        }
+    }
+    // -----location動畫-----
+    function loacationTitle(){
+        let outerHeight = $('.location > .title_box > .item').outerHeight();
+        let offsetTop = $('.location > .title_box > .item').offset().top;
+        if(scrollY + innerHeight - outerHeight * 0.9 > offsetTop){
+            $('.location .title_box > .item').addClass('active')
+        }else{
+            $('.location .title_box > .item').removeClass('active')
+        }
+    }
+    function schoolLocation(){
+        let outerHeight = $('.location').outerHeight();
+        let offsetTop = $('.location').offset().top;
+        if(scrollY + innerHeight - outerHeight / 3 > offsetTop){
+            $('.line1').addClass('active')
+            $('.school').addClass('active')
+        }
+        else{
+            $('.line1').removeClass('active')
+            $('.school').removeClass('active')
+        }
+
+    }
+    function shopLoaction(){
+        let outerHeight = $('.location').outerHeight();
+        let offsetTop = $('.location').offset().top;
+        if(scrollY + innerHeight - outerHeight / 3 * 2 > offsetTop){
+            $('.shop').addClass('active')
+            $('.line2').addClass('active')
+        }
+        else{
+            $('.shop').removeClass('active')
+            $('.line2').removeClass('active')
+        }
+    }
+    // ----discount動畫-----
+    // ---一般效果---
+    // function discount(){
+    //     let outerHeight = $('.discount').outerHeight();
+    //     let offsetTop = $('.discount').offset().top;
+    //     if(scrollY + innerHeight - outerHeight / 3 > offsetTop){
+    //         $('.discount > .item').addClass('active')
+    //     }
+    //     else{
+    //         $('.discount > .item').removeClass('active')
+    //     }
+    // }
+    // ---彈跳效果----
+    function discount(){
+        let outerHeight = $('.discount').outerHeight();
+        let offsetTop = $('.discount').offset().top;
+        if(scrollY + innerHeight - outerHeight > offsetTop){
+            $('.discount > .item').animate({
+                left:'50%',
+            },100,'easeOutQuint')
+        }
+        else{
+            $('.discount > .item').animate({
+                left:"-100%",
+            },2000,'easeOutQuint')
+        }
+    }
+    // -----slogan 輪播圖-----
+    let carouselBtn = document.querySelectorAll("input.carousel_btn");
+    function carousel(i){
+        setTimeout(
+            function(){
+                carouselBtn[i].click();
+                i++;
+                if(i <= 2){
+                    carousel(i);
+                }
+                if(i > 2){
+                    let i = 0;
+                    carousel(i);
+                }
+            }
+            ,4000
+        );
+    }
+    }
+)
 // index.html slogan動畫
 window.addEventListener("scroll", function(){
-  let slidein = document.getElementsByClassName("slidein");
-  for(let i = 0; i < slidein.length; i++){
-      if( ((this.scrollY + this.innerHeight) - slidein[i].offsetHeight / 3) > slidein[i].offsetTop){             
-          slidein[i].classList.add("active");
-      }else{
-          slidein[i].classList.remove("active");
-      };
-  }
-
-  let fadein = document.getElementsByClassName("fadein");
-  for(let i = 0; i < fadein.length; i++){
-      if( ((this.scrollY + this.innerHeight) - fadein[i].offsetHeight / 3) > fadein[i].offsetTop){          
-          fadein[i].classList.add("active");
-      }else{
-          fadein[i].classList.remove("active");
-      };
-  }
 // slogan輪播圖
 let carouselBtn = document.querySelectorAll("input.carousel_btn");
 
@@ -36,7 +156,7 @@ function carousel(i){
         ,4000
     );
 }
-window.addEventListener("load",carousel(1));
+// window.addEventListener("load",carousel(1));
 
   
   // ----loacation background-------
@@ -59,6 +179,8 @@ window.addEventListener("load",carousel(1));
           })
   })
 });
+// index.html location 動畫
+
 
 // --menu.html 換介紹---
 $(document).ready(function(){
